e4a91b6998a3c4d74148961bdda2dc97ea8cf8ee
fyosetake <fyosetake@gmail.com>
2023-09-14 22:44:03 -0300

    Constrói uma Hamburgueria virtual que envia o lanche via 'RabbitMQ'

---
+++ HamburguerProducer.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/HamburguerFactory.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class HamburguerProducer
+{
+    private $connection;
+    private $channel;
+
+    public function __construct()
+    {
+        $this->connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $this->channel = $this->connection->channel();
+        $this->channel->queue_declare('hamburguer_queue', false, false, false, false);
+    }
+
+    public function produceHamburguer($type)
+    {
+        $hamburguer = HamburguerFactory::createHamburguer($type);
+        $message = new AMQPMessage($hamburguer->getDescription());
+
+        $this->channel->basic_publish($message, '', 'hamburguer_queue');
+        echo "Produzido: ", $hamburguer->getDescription(), "\n";
+    }
+}
+
+$producer = new HamburguerProducer();
+
+while (true) {
+    echo "Digite o tipo de hambúrguer ('cheese' ou 'chicken') ou 'exit' para sair: ";
+    $input = trim(fgets(STDIN));
+
+    if ($input === 'exit') {
+        break;
+    }
+
+    if ($input === 'cheese' || $input === 'chicken') {
+        $producer->produceHamburguer($input);
+    } else {
+        echo "Tipo de hambúrguer inválido. Tente novamente.\n";
+    }
+}
